0a2ed9489893a255d47ec2595756977deaa9f369
Billy Lamug <Billf0rd@Billf0rds-MBPR.local>
2014-10-27 20:21:59 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Creature.h
@@ -0,0 +1,29 @@
+//
+//  Creature.h
+//  GameOfLife
+//
+//  Created by Billy Lamug on 10/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Creature : CCSprite
+
+// stores the current state of the creature
+@property (nonatomic, assign) BOOL isAlive;
+
+// stores the amount of living neighbors
+@property (nonatomic, assign) NSInteger livingNeighbors;
+
+- (id)initCreature;
+
+- (void)setIsAlive:(BOOL)newState {
+    //when you create an @property as we did in the .h, an instance variable with a leading underscore is automatically created for you
+    _isAlive = newState;
+    
+    // 'visible' is a property of any class that inherits from CCNode. CCSprite is a subclass of CCNode, and Creature is a subclass of CCSprite, so Creatures have a visible property
+    self.visible = _isAlive;
+}
+
+@end
